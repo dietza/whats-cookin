12a174481a186f2771706375418009e90cb23ae6
Allison Dietz <dietz.allison@outlook.com>
2020-10-13 19:07:46 -0600

    Build properties in Recipe class, add and pass tests

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,6 +1,11 @@
 class Recipe {
-  constructor() {
-    
+  constructor(recipe) {
+    this.id = recipe.id;
+    this.name = recipe.name;
+    this.image = recipe.image;
+    this.ingredients = recipe.ingredients;
+    this.instructions = recipe.instructions;
+    this.tags = recipe.tags;
   }
 }
 
--- tests/Recipe-test.js
+++ tests/Recipe-test.js
@@ -747,4 +747,116 @@ describe('Recipe', () => {
     let recipe = new Recipe(sampleRecipes[1]);
     expect(recipe).to.be.an.instanceof(Recipe);
   });
+
+  it('should have an id number', function() {
+    let recipe = new Recipe(sampleRecipes[2]);
+    expect(recipe.id).to.deep.equal(412309);
+  });
+
+  it('should have a name', function() {
+    let recipe = new Recipe(sampleRecipes[0]);
+    expect(recipe.name).to.deep.equal("Loaded Chocolate Chip Pudding Cookie Cups");
+  });
+
+  it('should have an associated image', function() {
+    let recipe = new Recipe(sampleRecipes[1]);
+    expect(recipe.image).to.deep.equal("https://spoonacular.com/recipeImages/678353-556x370.jpg");
+  });
+
+  it('should have a list of ingredients', function() {
+    let recipe = new Recipe(sampleRecipes[2]);
+    expect(recipe.ingredients).to.deep.equal([
+      {
+        "id": 1002030,
+        "quantity": {
+          "amount": 4,
+          "unit": "teaspoons"
+        }
+      },
+      {
+        "id": 19334,
+        "quantity": {
+          "amount": 8,
+          "unit": "tablespoons"
+        }
+      },
+      {
+        "id": 1001,
+        "quantity": {
+          "amount": 2,
+          "unit": "cups"
+        }
+      },
+      {
+        "id": 4582,
+        "quantity": {
+          "amount": 4,
+          "unit": "servings"
+        }
+      },
+      {
+        "id": 2031,
+        "quantity": {
+          "amount": 4,
+          "unit": "teaspoons"
+        }
+      },
+      {
+        "id": 5100,
+        "quantity": {
+          "amount": 1,
+          "unit": "pound"
+        }
+      },
+      {
+        "id": 2009,
+        "quantity": {
+          "amount": 4,
+          "unit": "teaspoons"
+        }
+      },
+      {
+        "id": 1022020,
+        "quantity": {
+          "amount": 4,
+          "unit": "teaspoons"
+        }
+      },
+      {
+        "id": 6168,
+        "quantity": {
+          "amount": 8,
+          "unit": "cups"
+        }
+      },
+      {
+        "id": 9176,
+        "quantity": {
+          "amount": 0.5,
+          "unit": "cup"
+        }
+      },
+      {
+        "id": 2026,
+        "quantity": {
+          "amount": 4,
+          "unit": "teaspoons"
+        }
+      },
+      {
+        "id": 1042047,
+        "quantity": {
+          "amount": 1.5,
+          "unit": "tablespoons"
+        }
+      },
+      {
+        "id": 1042047,
+        "quantity": {
+          "amount": 4,
+          "unit": "teaspoons"
+        }
+      }
+    ]);
+  });
 });
