175c3b13f89ce0bc2c613062bbfeadada99a83fd
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-10-19 13:22:01 -0500

    Reformat welcome page

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,34 @@
-console.log('Hello world');
+// ************GLOBAL VARIABLES***************//
+currentUser
+currentRecipeRepository
+
+// ****************NAVIGATION*******************  //
+
+// let allRecipesBtn = document.querySelector(".show-all-btn");
+// let filterBtn = document.querySelector(".filter-btn");
+// let pantryBtn = document.querySelector(".my-pantry-btn");
+// let savedRecipesBtn = document.querySelector(".saved-recipes-btn");
+let searchBtn = document.querySelector(".fa-search");
+let searchForm = document.querySelector("searchForm");
+let searchInput = document.querySelector("search-input");
+let showPantryRecipes = document.querySelector(".show-pantry-recipes-btn");
+let dropdown = document.querySelector(".hamburger-button-dropbtn")
+
+// ************EVENT HANDLERS****************//
+
+
+
+
+//*****************ONLOAD******************//
+
+function uploadData() {
+  currentUser = new User(userData[getRandomIndex(userData)])
+  currentRecipeRepository = new recipeRepository(recipeData)
+}
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+
+//*****************ONLOAD********************//
