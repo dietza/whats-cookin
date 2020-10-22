042b230222519a8750c322164652e568133ad939
Allison Dietz <dietz.allison@outlook.com>
2020-10-21 19:43:49 -0600

    Move script tags to resolve undefined querySelectors, display recipe ingredients upon clicking 'my pantry'

--- src/scripts.js
+++ src/scripts.js
@@ -9,7 +9,6 @@ let allRecipes = [];
 let allRecipesBtn = document.querySelector(".all-recipes-button");
 let currentViewTitle = document.querySelector(".current-view-title");
 
-
 let searchBtn = document.querySelector(".fa-search");
 let searchForm = document.querySelector(".searchForm");
 let searchInput = document.querySelector(".search-input");
@@ -20,116 +19,72 @@ let myToCookBtn = document.querySelector(".to-cook");
 let myCookedBtn = document.querySelector(".cooked-recipes");
 
 let recipeDisplay = document.querySelector(".main-recipe-display");
-let allRecipeCards = document.querySelectorAll(".recipe-card")
-// let recipeCard1 = document.querySelector(".card-1");
+let allRecipeCards = document.querySelectorAll(".recipe-card");
 
 
 // ************EVENT LISTENERS***************//
 window.addEventListener('load', function () {
- uploadData()
+ uploadData();
+
+ myPantryBtn.addEventListener('click', displayPantry);
+
+ myFavoritesBtn.addEventListener('click', displayFavorites);
+
+ myToCookBtn.addEventListener('click', displayRecipesToCook);
+
+ myCookedBtn.addEventListener('click', displayCookedRecipes);
+
 })
 
-
 // ************EVENT HANDLERS****************//
-myPantryBtn.addEventListener('click', displayPantry);
-
-myFavoritesBtn.addEventListener('click', displayFavorites);
-
-myToCookBtn.addEventListener('click', displayRecipesToCook);
-
-myCookedBtn.addEventListener('click', displayCookedRecipes);
+// myPantryBtn.addEventListener('click', displayPantry);
+//
+// myFavoritesBtn.addEventListener('click', displayFavorites);
+//
+// myToCookBtn.addEventListener('click', displayRecipesToCook);
+//
+// myCookedBtn.addEventListener('click', displayCookedRecipes);
 
 //*****************ONLOAD******************//
 function uploadData() {
   currentUser = new User(usersData[getRandomIndex(usersData)], ingredientsData);
-
-  console.log('uploadData // currentUSER:', currentUser);
-  console.log('displayUserData() :', displayUserData());
-
   createRecipeObjects(recipeData);
   getRandomIndex(ingredientsData);
   displayUserData();
   displayRecipeDetails(allRecipes);
-}
+};
 
 function createRecipeObjects(recipeData) {
   allRecipes = recipeData.map(recipe => {
     return new Recipe(recipe);
   })
   return allRecipes;
-}
+};
 
 //*****************MISC // INSTANTIATIONS****************//
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
-}
+};
 
 function numToName(ingredientID, ingredientsData) {
   let ingredient = ingredientsData.find(ingredient => ingredient.id === ingredientID)
   return ingredient.name;
-}
+};
 
 //*****************DISPLAY****************//
 
 function displayUserData() {
-
-  console.log('displayUserData // currentUSER:', currentUser);
-
   let currentFirstName = currentUser.displayFirstName();
   document.querySelector('.welcome-bar').innerHTML =
     `<h1>Welcome,</h1>
     <h3> ${currentUser.displayFirstName()}! </h3>`
-}
-
-// function displayRecipeCards(allRecipes) {
-//   // recipeDisplay.innerHTML = "";
-//
-//
-//
-//   let recipeListIngredients = recipeData[0].ingredients.map(ingredient => {
-//     return ` ${numToName(ingredient.id, ingredientsData)}`;
-//   });
-//
-//   let recipeListInstructions = recipeData[0].instructions.map(instruction => {
-//     return `<br>${instruction.number}. ${instruction.instruction}`;
-//   })
-//
-//   console.log('displayRecipeCards // recipeListIngredients:', recipeListIngredients);
-//
-//   document.querySelector('.card-1').innerHTML =
-//   `<div class="card-1 recipe-card default-recipe" name="recipe 1">
-//     <div class='recipe-image'><img src=${recipeData[0].image}></div>
-//     <div class='recipe-name'><h1>${recipeData[0].name}</h1></div>
-//     <!-- <div class='recipe-ingredients'><p>${recipeListIngredients}</p></div> -->
-//     <!-- <div class='recipe-instructions'><h3>${recipeListInstructions}</h3></div> -->
-//   </div>`
-//
-// };
-
-  // let recipeListIngredients = recipeData[0].ingredients.map(ingredient => {
-  //   return ` ${numToName(ingredient.id, ingredientsData)}`;
-  // });
-  //
-  // let recipeListInstructions = recipeData[0].instructions.map(instruction => {
-  //   return `<br>${instruction.number}. ${instruction.instruction}`;
-  // })
-  //
-  // console.log('displayRecipeCards // recipeListIngredients:', recipeListIngredients);
-  //
-  // document.querySelector('.card-1').innerHTML =
-  // `<div class="card-1 recipe-card default-recipe" name="recipe 1">
-  //   <div class='recipe-image'><img src=${recipeData[0].image}></div>
-  //   <div class='recipe-name'><h1>${recipeData[0].name}</h1></div>
-  //   <!--<div class='recipe-ingredients'><p>${recipeListIngredients}</p></div> -->
-  //   <!--<div class='recipe-instructions'><h3>${recipeListInstructions}</h3></div> -->
-  // </div>`
-
+};
 
 function addRecipeImage(recipe) {
   document.getElementById('#recipe-title').style.backgroundImage =
     `url(${recipe.image})`
-}
+};
 
 function displayRecipeDetails(allRecipes) {
   allRecipes.forEach(recipe => {
@@ -152,18 +107,15 @@ function displayRecipeDetails(allRecipes) {
 };
 
 function displayPantry() {
-
   let ingredientNames = currentUser.pantry.forEach(ingredient => {
-    let ingredientName = numToName(ingredientID, ingredientsData);
+    let ingredientName = numToName(ingredient.ingredient, ingredientsData);
     let main = document.querySelector('.main-recipe-display');
     let pantryHTML = `
     <li><input type="checkbox" class="pantry-checkbox" id="${ingredient.id}">
       <label for="${ingredientName}">${ingredientName},
       ${ingredient.amount}</label></li>`
-
-      document.querySelector('pantry-list').insertAdjacentHTML('beforeEnd', pantryHTML)
+    main.insertAdjacentHTML('beforeEnd', pantryHTML)
   })
-
 };
 
 function displayFavorites() {
